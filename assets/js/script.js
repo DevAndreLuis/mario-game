5dc149a742117ece12c14879f7defe7c55a1e60d
DevAndreLuis <andredev.github@gmail.com>
2024-02-12 00:07:28 -0300

    Alterações nas nuvens e também na suavidade do pulo do mario e algumas srites

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,15 +11,27 @@ function jump() {
     setTimeout(function () {
       mario.classList.remove("pulo");
       isJumping = false;
-    }, 800);
+    }, 700);
   }
 }
 
 const loop = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+  console.log(marioPosition);
 
-  if (pipePosition <= 65) {
+  if (pipePosition <= 70 && pipePosition > 0 && marioPosition < 84) {
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+    mario.src = "./assets/images/game-over.png";
+    mario.style.width = "50px";
+    mario.style.marginLeft = "25px";
+
+    clearInterval(loop);
   }
-}, 15);
+}, 25);
